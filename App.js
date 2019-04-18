d6554783182cee3216aac9e4d2e1fae8baa20489
Fahad Mahmood <fahad492@outlook.com>
2019-04-18 12:32:17 +0500

    React Native PlayGround
    Playing with react native on snack!!

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.c1}>Welcome Chaudhry</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  c1:{
+    color:'red',
+  }
+});
